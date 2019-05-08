f8e44172950a0f8795aef08e17d95e048d8bb48c
Filipe PARENTE <lfmparente@gmail.com>
2019-05-08 12:50:02 +0200

    NgRx Feature Design - Defining the Actions First

---
+++ src/app/courses/store/course.actions.ts
@@ -0,0 +1,22 @@
+import { Action } from '@ngrx/store';
+import { Course } from '../model/course';
+
+export enum CourseActionTypes {
+    CourseRequested = '[View course Page] Course requested',
+    CourseLoaded = '[Course API] Course Loaded'
+}
+
+export class CourseRequested implements Action {
+    readonly type = CourseActionTypes.CourseRequested;
+
+    constructor(public payload: { courseId: number }) { }
+}
+
+export class CourseLoaded implements Action {
+    readonly type = CourseActionTypes.CourseLoaded;
+
+    constructor(public payload: { course: Course }) { }
+}
+
+
+export type CourseActions = CourseRequested | CourseLoaded;
