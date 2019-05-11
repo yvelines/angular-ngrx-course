734bb8e665372eb9856a568868aba940801121ef
Filipe PARENTE <lfmparente@gmail.com>
2019-05-11 14:17:25 +0200

    Implementing the Lessons Page Selector

---
+++ src/app/courses/store/lessons.selectors.ts
@@ -0,0 +1,31 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+
+import { PageQuery } from './lessons.actions';
+import * as fromLessons from './lessons.reducers';
+import { Lesson } from '../model/lesson';
+
+
+const calcPagination = (page: PageQuery): [number, number] => {
+    return [
+        page.pageIndex * page.pageSize,
+        (page.pageIndex * page.pageSize) + page.pageSize
+    ];
+};
+
+export const selectLessonstate = createFeatureSelector<fromLessons.LessonState>('lessons');
+
+export const selectAllLessons = createSelector(
+    selectLessonstate,
+    fromLessons.selectAll
+);
+
+export const selectLessonsPage = (courseId: number, page: PageQuery) => createSelector(
+    selectAllLessons,
+    allLessons => {
+        const [start, end] = calcPagination(page);
+        allLessons.filter((lesson: Lesson) => lesson.courseId === courseId).slice(start, end);
+    }
+);
+
+
+
